ec933862c23dac2dc0369f15c84a4f26e011e6b5
leandro1989 <leandrolinken@gmail.com>
2016-10-11 16:51:05 -0300

    Servidor atualizado, falta copilar

--- jogoDaVelha-servidor.py
+++ jogoDaVelha-servidor.py
@@ -13,13 +13,14 @@ tabuleiro_aux = ['','1','2','3','4','5','6','7','8','9','2',' ']
 
 jogadas_possiveis = ['1','2','3','4','5','6','7','8','9']
 
-jogadas = 0
-jogador_da_vez = 0
-vitorias_um = 0
-vitorias_dois = 0
-empates = 0
+jogadas = 0  #Contador de jogadas
+vitorais_servidor = 0 #Contador de vitorias do jogador servidor
+vitorais_cliente = 0 #Contador de vitorias do jogador cliente
+empates = 0 #Contador de empates
 
 def imprime(tabuleiro):
+    '''Interface'''
+
     print('')
     print('\t\t      |     |     ')
     print('\t\t   %s  |  %s  |  %s '%(tabuleiro[7],tabuleiro[8],tabuleiro[9]))
@@ -32,176 +33,204 @@ def imprime(tabuleiro):
     print('\t\t      |     |     ')
     print('')
 
-def jogada(posicao,jogador_vez):
-    while True:
-        if tabuleiro[posicao] not in [simbolo_jogador1,simbolo_jogador2]:
+def jogada(posicao, jogador_vez):
+    '''Verifica se a posição já foi jogada e realiza a jogada'''
 
-            if jogador_vez == 1:
-                tabuleiro[posicao] = simbolo_jogador1
-                return 1
-            elif jogador_vez == 2:
-                tabuleiro[posicao] = simbolo_jogador2
-                return 2
+    while True:
+
+        if tabuleiro[posicao] not in [simbolo_jogador_servidor, simbolo_jogador_cliente]:
+
+            if jogador_vez == 'servidor':
+                tabuleiro[posicao] = simbolo_jogador_servidor
+                return True
+
+            elif jogador_vez == 'cliente':
+                tabuleiro[posicao] = simbolo_jogador_cliente
+                return True
+
             break
+
         else:
-            imprime(tabuleiro_aux)
-            imprime(tabuleiro)
-            posicao = int(input("Posição ocupada, tente outra posição:  "))
+            return False
+
 
 def vencedor():
-    i = 1
-    v = 1
-    
-    while i <= 7:
-        if tabuleiro[i] == tabuleiro[i+1] and tabuleiro[i+1] == tabuleiro[i+2] and tabuleiro[i+2] == simbolo_jogador1: #ganhou na horizontal
-            i = 1
-            return 1
+    '''Verifica se tem algum vencedor'''
+    horizontal = 1
+    vertical = 1
+
+    while horizontal <= 7:
+
+        if tabuleiro[horizontal] == tabuleiro[horizontal + 1] and tabuleiro[horizontal + 1] == tabuleiro[
+                    horizontal + 2] and tabuleiro[horizontal + 2] == simbolo_jogador_cliente:  # ganhou na horizontal
+            horizontal = 1
+            return 'cliente'
             break
 
-        elif tabuleiro[i] ==tabuleiro[i+1] and tabuleiro[i+1] ==tabuleiro[i+2] and tabuleiro[i+2] == simbolo_jogador2: #ganhou na horizontal
-            i = 1
-            return 2
+        elif tabuleiro[horizontal] == tabuleiro[horizontal + 1] and tabuleiro[horizontal + 1] == tabuleiro[
+                    horizontal + 2] and tabuleiro[horizontal + 2] == simbolo_jogador_servidor:  # ganhou na horizontal
+            horizontal = 1
+            return 'servidor'
             break
 
-        aux = v + 3
-        if tabuleiro[v] == tabuleiro[v+3] and tabuleiro[v] == tabuleiro[aux+3] and tabuleiro[aux+3] == simbolo_jogador1:
-            i = 1
-            return 1
+        aux = vertical + 3
+        if tabuleiro[vertical] == tabuleiro[vertical + 3] and tabuleiro[vertical] == tabuleiro[aux + 3] and tabuleiro[
+                    aux + 3] == simbolo_jogador_cliente:  # ganhou na vertical
+            horizontal = 1
+            return 'cliente'
             break
 
-        elif tabuleiro[v] == tabuleiro[v+3] and tabuleiro[v] == tabuleiro[aux+3] and tabuleiro[aux+3]  == simbolo_jogador2:
-            i = 1
-            return 2
+        elif tabuleiro[vertical] == tabuleiro[vertical + 3] and tabuleiro[vertical] == tabuleiro[aux + 3] and tabuleiro[
+                    aux + 3] == simbolo_jogador_servidor:  # ganhou na vertical
+            horizontal = 1
+            return 'servidor'
             break
 
-        #diagonal 1
-        if tabuleiro[7] == tabuleiro[5] and tabuleiro[5] == tabuleiro[3] and tabuleiro[3] == simbolo_jogador1:
-            i = 1
-            return 1
+        if tabuleiro[7] == tabuleiro[5] and tabuleiro[5] == tabuleiro[3] and tabuleiro[
+            3] == simbolo_jogador_cliente:  # ganhou na diagonal 1
+            horizontal = 1
+            return 'cliente'
             break
 
-        elif tabuleiro[7] == tabuleiro[5] and tabuleiro[5] == tabuleiro[3] and tabuleiro[3] == simbolo_jogador2:
-            i = 1
-            return 2
+        elif tabuleiro[7] == tabuleiro[5] and tabuleiro[5] == tabuleiro[3] and tabuleiro[
+            3] == simbolo_jogador_servidor:  # ganhou na diagonal 1
+            horizontal = 1
+            return 'servidor'
             break
 
-        #diagonal 2
-        if tabuleiro[9] == tabuleiro[5] and tabuleiro[5] == tabuleiro[1] and tabuleiro[1] == simbolo_jogador1:
-            i = 1
-            return 1
+        if tabuleiro[9] == tabuleiro[5] and tabuleiro[5] == tabuleiro[1] and tabuleiro[
+            1] == simbolo_jogador_cliente:  # ganhou na diagonal 2
+            horizontal = 1
+            return 'cliente'
             break
 
-        elif tabuleiro[9] == tabuleiro[5] and tabuleiro[5] == tabuleiro[1] and tabuleiro[1] == simbolo_jogador2:
-            i = 1
-            return 2
+        elif tabuleiro[9] == tabuleiro[5] and tabuleiro[5] == tabuleiro[1] and tabuleiro[
+            1] == simbolo_jogador_servidor:  # ganhou na diagonal 2
+            horizontal = 1
+            return 'servidor'
             break
 
-        i += 3
-        v += 1
-    return 'Sem Vencedor'
+        horizontal += 3
+        vertical += 1
 
 def jogar_novamente(): #como estava repetindo muito criei está função
+    '''Verifica se os jogadores querem jogar novamente'''
 
     print('')
-    escolha = input("Digite S se quer jogar novamente? Caso contrário, aperte outra tecla: ")
+    escolha = input('Digite S se quer jogar novamente? Caso contrário, aperte outra tecla: ')
     envia_escolha = bytes(escolha)
-    conecxao.send(escolha)
+    conecxao.send(envia_escolha)
 
     print('')
-    print('Esperando a resposicaota do adversário! Aguarde...')
+    print('Esperando a resposta de %s! Aguarde...' %jogador_cliente)
     resposta = conecxao.recv(1024)
 
     if escolha.upper() == 'S' and resposta == 'S':
-        global jogadas
+
+        global jogadas #Renova a contagem de jogadas
         jogadas = 1
+
         return True
 
     else:
+
         print('O outro jogador desistiu. Fim de Jogo')
         print('')
+        print('Resultado do game: ')
+        print('')
         print('Empates: ', empates)
-        print('Vitórias de %s = %d' %(jogador1, vitorias_um))
-        print('Vitórias de %s = %d' %(jogador2, vitorias_dois))
+        print('Vitórias de %s = %d' %(jogador_servidor, vitorais_servidor))
+        print('Vitórias de %s = %d' %(jogador_cliente, vitorais_cliente))
 
 while True:
     
     while True:
         
-        jogador1 = input('Informe o nome do jogar que vai jogar primeiro: ')
+        jogador_servidor = input('Informe o nome do jogar que vai jogar primeiro: ')
     
-        if not jogador1.isalpha():
+        if not jogador_servidor.isalpha():
+
             print('Informe seu nome corretamente.')
             print('')
+
         else:
+
             print('')
-            print('Esperando o nome do jogador adversário! Aguarde...')
+            print('Esperando o jogador adversário! Aguarde...')
             conecxao, cliente = tcp.accept()
 
-            envia_nome = jogador1
+            envia_nome = jogador_servidor
             envia_nome = bytes(envia_nome)
             conecxao.send(envia_nome)
-            jogador2 = conecxao.recv(1024)
+
+            print('Esperando o nome do jogador adversário! Aguarde...')
+            jogador_cliente = conecxao.recv(1024)
+            jogador_cliente = str(jogador_cliente,'utf-8')
+
             break
     
     while True:
     
-        simbolo_jogador1 = input('%s Informe se você vai jogar: '%(jogador1))
-        simbolo_jogador1 = simbolo_jogador1.upper()
+        simbolo_jogador_servidor = input('%s Informe se você vai jogar: '%(jogador_servidor))
+        simbolo_jogador_servidor = simbolo_jogador_servidor.upper()
         
-        enviar_simbolo = simbolo_jogador1
+        enviar_simbolo = simbolo_jogador_servidor
         enviar_simbolo = bytes(enviar_simbolo)
         conecxao.send(enviar_simbolo)
         
         print('Esperando o símbolo que será usado pelo adversário! Aguarde...')
-        simbolo_jogador2 = conecxao.recv(1024)
-        simbolo_jogador2 = str(simbolo_jogador2,'utf-8')
+        simbolo_jogador_cliente = conecxao.recv(1024)
+        simbolo_jogador_cliente = str(simbolo_jogador_cliente,'utf-8')
     
-        if simbolo_jogador1 == simbolo_jogador2:
+        if simbolo_jogador_servidor == simbolo_jogador_cliente:
+
             print('O outro jogador escolheu o mesmo Símbolo que você. Informe outro símbolo para continuar o jogo.')
             print('')
+
         else:
-            simbolo_jogador1 = colored(simbolo_jogador1, 'red')
-            simbolo_jogador2 = colored(simbolo_jogador2, 'blue')
+
+            simbolo_jogador_servidor = colored(simbolo_jogador_servidor, 'red')
+            simbolo_jogador_cliente = colored(simbolo_jogador_cliente, 'blue')
             print('')
             break
 
-    jogador_vez = 1
+    jogador_vez = 'servidor'
 
-    while jogador_vez == 1:
-
+    while jogador_vez == 'servidor':
+        
+        jogador_vez = 'servidor'
         imprime(tabuleiro_aux)
         imprime(tabuleiro)
 
-        posicao = input("Escolha, utilizando os números de 1 a 9, em qual posição vai jogar %s: " %jogador1)
+        posicao = input('Escolha, utilizando os números de 1 a 9, em qual posição vai jogar %s: ' %jogador_servidor)
 
-        if posicao in jogadas_possiveis:
+        if posicao in jogadas_possiveis and jogada(posicao, jogador_vez) == True:
+
             posicao = int(posicao)
-
             posicao_enviada = str(posicao)
-            posicao_enviada= bytes(posicao_enviada)
+            posicao_enviada = bytes(posicao_enviada)
             conecxao.send(posicao_enviada)  #O cliente vai receber a posição, verificar em vencedor() e retornar o resultado
 
-            jogada(posicao, jogador_vez)
-
-            jogador_vez = 2
+            jogador_vez = 'cliente'
 
         else:
+
             imprime(tabuleiro_aux)
             imprime(tabuleiro)
             print('Posição Inválida. Informe um número de 1 a 9.')
-            continue
 
     imprime(tabuleiro_aux)
     imprime(tabuleiro)
-
-    venceu = conecxao.recv(1024)
-    venceu = str(venceu,'utf-8')
-    venceu = int(venceu)
     jogadas += 1
 
-    if venceu == 1:
-        print("Parabéns %s! Você GANHOU!!" %jogador1)
-        vitorias_um += 1
+    venceu = vencedor()
+
+    if venceu == 'servidor':
+
+        imprime(tabuleiro_aux)
+        imprime(tabuleiro)
+        print('Parabéns %s! Você GANHOU!!' %jogador_servidor)
+        vitorais_servidor += 1
 
         if jogar_novamente() == True:
             continue
@@ -209,7 +238,10 @@ while True:
             break
 
     elif jogadas == 9:
-        print("Partida empatada,nenhum vencendor!")
+
+        imprime(tabuleiro_aux)
+        imprime(tabuleiro)
+        print('Partida empatada,nenhum vencendor!')
         empates += 1
 
         if jogar_novamente() == True:
@@ -217,33 +249,43 @@ while True:
         else:
             break
 
-    print('Esperando o adversário jogar! Aguarde...')
+    print('Esperando o %s jogar! Aguarde...' %jogador_cliente)
+
+    conecxao.send(b'cliente')
 
     posicao_adversario = conecxao.recv(1024) #recebe a posição que o adversário vai jogar
     posicao_adversario = str(posicao_adversario,'utf-8')
     posicao_adversário = int(posicao_adversario)
 
     jogada(posicao_adversario, jogador_vez)
+    jogadas += 1
+
     imprime(tabuleiro_aux)
     imprime(tabuleiro)
 
     venceu = vencedor()
-    jogadas += 1
 
-    if venceu == 2:
-        print("O jogador %s Ganhou! Você Perdeu!!" %jogador1)
-        vitorias_um += 1
+    if venceu == 'cliente':
+
+        imprime(tabuleiro_aux)
+        imprime(tabuleiro)
+        print('O jogador %s Ganhou! Você Perdeu!!' %jogador_servidor)
+        vitorais_cliente += 1
 
         if jogar_novamente() == True:
             continue
         else:
             break
 
-    elif jogadas == 9:
-        print("Partida empatada,nenhum vencendor!")
+    elif jogadas == 9: #O total de jogadas é 9
+
+        imprime(tabuleiro_aux)
+        imprime(tabuleiro)
+        print('Partida empatada,nenhum vencendor!')
         empates += 1
 
         if jogar_novamente() == True:
             continue
+
         else:
             break
